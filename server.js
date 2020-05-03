40988c455ca9e35ee4e9517be90f2497bda4ef5a
Lacey Boblak <laceyannmadison@gmail.com>
2020-05-03 14:17:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 if(process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
+  express.use((req, res, next) => {
     if (req.header('x-forwarded-proto') !== 'https')
       res.redirect(`https://${req.header('host')}${req.url}`)
     else
